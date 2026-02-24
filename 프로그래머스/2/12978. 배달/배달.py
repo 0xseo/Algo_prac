ae0b2fb11d6b8c93903cfed01f59d53083c8c5d4
0xseo <87014012+0xseo@users.noreply.github.com>
2026-02-24 17:51:08 +0900

    Refactor delivery algorithm using adjacency matrix

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -3,30 +3,35 @@ def solution(N, road, K):
     # heapq.heappush(리스트, 값)
     # heapq.heappop(리스트)
     inf = 9e9
-    hq = []
-    distances = [inf for _ in range(N+1)]
-    distances[1] = 0
-    canGo = [[] for _ in range(N+1)]
+    graph = [[-1 for _ in range(N + 1)] for _ in range(N + 1)]
     
-    for s, e, c in road:
-        canGo[s].append([e, c])
-        canGo[e].append([s, c])
-        
-    for togo in canGo[1]:
-        heapq.heappush(hq, togo)
+    dist = [inf] * (N+1)
+    dist[1] = 0
     
+    for edge in road:
+        if graph[edge[0]][edge[1]] == -1:
+            graph[edge[0]][edge[1]] = edge[2]
+        else:
+            graph[edge[0]][edge[1]] = min(graph[edge[0]][edge[1]], edge[2])
+        if graph[edge[1]][edge[0]] == -1:
+            graph[edge[1]][edge[0]] = edge[2]
+        else:
+            graph[edge[1]][edge[0]] = min(graph[edge[1]][edge[0]], edge[2])
+    
+    h = []
+    heapq.heappush(h, (0, 1))
+    
+    while h:
+        cost, node = heapq.heappop(h)
+
+        for i, t in enumerate(graph[node]):
+            if t == -1:
+                continue
+            if cost + t <= dist[i]:
+                dist[i] = cost + t
+                heapq.heappush(h, (cost + t, i))
+    # print(dist)
     answer = 0
-    
-    while hq:
-        togo = heapq.heappop(hq)
-        if distances[togo[0]] > togo[1]:
-            distances[togo[0]] = togo[1]
-            for nextGo, nextCost in canGo[togo[0]]:
-                heapq.heappush(hq, [nextGo, nextCost + distances[togo[0]]])
-    
-    for d in distances:
-        if d <= K:
+    for c in dist[1:]:
+        if c <= K:
             answer += 1
-        
-        
-    return answer
