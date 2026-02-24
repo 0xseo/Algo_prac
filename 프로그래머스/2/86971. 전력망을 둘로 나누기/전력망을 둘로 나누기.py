1bb253d36081066c725a86794800a5124ec6e9a0
0xseo <87014012+0xseo@users.noreply.github.com>
2026-02-24 19:19:43 +0900

    Refactor power network division logic

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,26 +1,33 @@
-import math
 def solution(n, wires):
-    def union(a, b):
-        parents[find(a)] = parents[find(b)]
-    def find(a):
-        if parents[a] == a:
-            return a
-        parents[a] = find(parents[a])
-        return parents[a]
+    def cnt_cons(cons):
+        visited = [False] * (n+1)
+        q = [1]
+        while q:
+            cur = q.pop()
+            for i, com in enumerate(cons[cur]):
+                if com == 0 or i == cur:
+                    continue
+                if com == 1 and not visited[i]:
+                    q.append(i)
+                    visited[i] = True
+            # break
+
+        # print(cnt)
+        return sum(visited)
+    cons = [[0] * (n+1) for _ in range(n+1)]
+    answer = []
     
-    answer = set()
-    
-    for i, cutWire in enumerate(wires):
-        parents = [i for i in range(n)]
-        for j, w in enumerate(wires):
-            if i == j:
-                continue
-            union(w[0]-1, w[1]-1)
-        cnt = 0
-        for k in range(n):
-            if find(k) == find(0):
-                cnt += 1
-        answer.add(abs(n - 2*cnt))
+    for i, j in wires:
+        cons[i][j] = 1
+        cons[j][i] = 1
+    for i, j in wires:
+        cons[i][j] = 0
+        cons[j][i] = 0
+        
+        answer.append(abs(2 * cnt_cons(cons) - n))
+                      
+        cons[i][j] = 1
+        cons[j][i] = 1
         
     return min(answer)
-        
+    
