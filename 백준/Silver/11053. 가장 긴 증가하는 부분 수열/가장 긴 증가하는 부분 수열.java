adbf8fbcde7cbbc828ed82c36e764c717dfd1bb9
0xseo <87014012+0xseo@users.noreply.github.com>
2026-03-30 15:15:32 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 128 ms, Memory: 15924 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -5,30 +5,36 @@ public class Main {
   static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
   static BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
   static StringTokenizer st;
+  static int[] arr;
+  static int[] dp;
+  public static int binarySearch(int left, int right, int target) {
+    int mid;
+    while (left < right) {
+      mid = (left + right) / 2;
+      if (dp[mid] < target) left = mid + 1;
+      else right = mid;
+    }
+    return right;
+  }
   public static void main(String[] args) throws IOException {
     st = new StringTokenizer(reader.readLine());
     int N = Integer.parseInt(st.nextToken());
-    int[] arr = new int[N];
-    int[] dp = new int[N];
-    int res = 0;
+    arr = new int[N];
+    dp = new int[N];
     st = new StringTokenizer(reader.readLine());
 
     for (int i = 0; i < N; i++) {
       arr[i] = Integer.parseInt(st.nextToken());
     }
 
+    int len = 0;
     for (int i = 0; i < N; i++) {
-      dp[i] = 1;
-      for (int j = 0; j < i; j++) {
-        if (arr[i] > arr[j]) {
-          dp[i] = Math.max(dp[i], dp[j]+1);
-        }
-      }
-      res = Math.max(res, dp[i]);
+      int idx = binarySearch(0, len, arr[i]);
+      dp[idx] = arr[i];
+      len = Math.max(len, idx+1);
     }
-
-    writer.write(res + "\n");
-
+    
+    writer.write(len + "\n");
     writer.flush();
   }
 
