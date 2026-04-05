4ab26f11274b156e98701c1f5465850d35db3504
0xseo <87014012+0xseo@users.noreply.github.com>
2026-04-05 20:14:27 +0900

    [Silver IV] Title: 수 찾기, Time: 44 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -2,16 +2,6 @@
 #include <algorithm>
 
 using namespace std;
-int b_search(int arr[], int size, int num, int l, int r) {
-  int mid = 0;
-  while (l <= r) {
-    mid = (r + l) / 2;
-    if (arr[mid] == num) return mid;
-    else if (arr[mid] > num) r = mid-1;
-    else l = mid+1;
-  }
-  return -1;
-}
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(NULL);
@@ -30,9 +20,9 @@ int main() {
   for (int i = 0; i < M; i++) {
     int num;
     cin >> num;
-    int fIndex = b_search(arr, N, num, 0, N-1);
-    if (fIndex == -1) cout << "0\n";
-    else cout << "1\n";
+    bool isIn = binary_search(arr, arr+N, num);
+    if (isIn) cout << "1\n";
+    else cout << "0\n";
   }
 
   return 0;
